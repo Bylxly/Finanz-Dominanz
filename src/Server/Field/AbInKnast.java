f5ee9ef254e6eb613b409f23c868cab243d7f8a2
timburke-lehmann <timburke@gmx.de>
2024-11-12 22:44:00 +0100

    Added override of Field Class

--- src/Server/Field/AbInKnast.java
+++ src/Server/Field/AbInKnast.java
@@ -1,8 +1,15 @@
 package Server.Field;
 
+import Server.Player;
+
 public class AbInKnast extends Field {
 
     public AbInKnast(String name) {
         super(name);
+    }
+
+    @Override
+    public void startAction(Player p) {
+
     }
 }
