ecb00ec47a8fe11c0e54abc9d2ff961eba135da2
plankeeee <skeley007@gmail.com>
2024-11-12 14:43:09 +0100

    implemented "Roll" class from UML diagram to roll the dice

---
+++ src/Server/Roll.java
@@ -0,0 +1,24 @@
+package Server;
+
+import java.util.Random;
+
+public class Roll {
+
+    private int number1;
+    private int number2;
+    private int total;
+    private Random rand = new Random();
+
+    public Roll() {
+
+    }
+
+    public void generate() {
+        number1 = rand.nextInt(6) + 1;
+        number2 = rand.nextInt(6) + 1;
+    }
+
+    public void calcTotal(){
+        total = number1 + number2;
+    }
+}
