dd13de7558fdf5bbf63840607b9a3afc6ed25e80
plankeeee <skeley007@gmail.com>
2024-11-12 14:46:16 +0100

    implemented "GameUtilities" class from UML diagram which contains all events a player can have

---
+++ src/Server/GameUtilities.java
@@ -0,0 +1,27 @@
+package Server;
+
+import java.util.ArrayList;
+
+public class GameUtilities {
+
+    public void transferMoney(Player fromPlayer, Player toPlayer, int amount){
+        fromPlayer.takeMoney(amount);
+        toPlayer.giveMoney(amount);
+    }
+
+    public void payBank(Player p, int amount){
+        p.takeMoney(amount);
+    }
+
+    public void receiveFromBank(Player p, int amount){
+        p.giveMoney(amount);
+    }
+
+    public void shuffleCards(ArrayList<Card> cards){
+
+    }
+
+    public void drawCard(ArrayList<Card> cards){
+
+    }
+}
