97d786cfdc4bc5541ee70bf10399f7ed5b8dc761
plankee <nikolas.dori@lgs-hu.eu>
2024-11-17 15:00:03 +0100

    implemented class "Street" from the UML diagram with association to "ColorGroup"

---
+++ src/Server/Field/Property/Street.java
@@ -0,0 +1,55 @@
+package Server.Field.Property;
+
+import Server.GameUtilities;
+import Server.Player;
+
+public class Street extends Property {
+
+    private int houses;
+    private boolean hasHotel = false;
+    private ColorGroup colorGroup;
+
+    public Street(String name, int price, int rent, int hypothek) {
+        super(name, price, rent, hypothek);
+        this.colorGroup = colorGroup;
+    }
+
+    @Override
+    public boolean startAction(Player player) {
+        if (GameUtilities.checkIfEnoughMoney(player, getRent())) {
+            super.payRent(player);
+            return true;
+        }
+        return false;
+    }
+
+    public void buildHouses(){
+        houses++;
+    }
+
+    public void buildHotel(){
+        hasHotel = true;
+    }
+
+    public void removeHouse(){
+        if (houses > 0){
+            houses--;
+        }
+    }
+
+    public void removeHotel(){
+        hasHotel = false;
+    }
+
+    public int getHouses(){
+        return houses;
+    }
+
+    public boolean getHasHotel(){
+        return hasHotel;
+    }
+
+    public ColorGroup getColorGroup() {
+        return colorGroup;
+    }
+}
