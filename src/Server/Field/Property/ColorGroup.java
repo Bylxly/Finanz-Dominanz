ddbe9565e5475f00f0d404c5a71c831be54b3fc6
plankee <nikolas.dori@lgs-hu.eu>
2024-11-17 14:59:48 +0100

    implemented class "ColorGroup" from the UML diagram with association to "Street"

---
+++ src/Server/Field/Property/ColorGroup.java
@@ -0,0 +1,37 @@
+package Server.Field.Property;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorGroup {
+    private String color;
+    private List<Street> streets;
+
+    public ColorGroup(String color) {
+        this.color = color;
+        this.streets = new ArrayList<>();
+    }
+
+    public void addStreet(Street street) {
+        if (!streets.contains(street)) {
+            streets.add(street);
+        }
+    }
+
+    public boolean isComplete() {
+        for (Street street : streets) {
+            if (!street.getHasHotel()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Street> getStreets() {
+        return streets;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
