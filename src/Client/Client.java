8ce5b98261d12313f40c627e1d0e8f51ff4ee3ec
samtr <samtrilin@gmail.com>
2024-11-18 11:31:59 +0100

    Added client files

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,19 @@
 package Client;
 
+import Server.State.GameState;
+
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+    private int playerID;
+    private boolean connected;
+    private GameState currentgameState;
+    private boolean isTurn;
+    private Draw draw;
+    private UI ui;
+    private int money;
+    private int figureID;
+    private boolean gameOver;
+    public Client() {}
 }
