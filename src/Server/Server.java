3d2fb629af947d50c1f192ac88df8731cf8262ef
timburke-lehmann <timburke@gmx.de>
2024-11-19 18:52:40 +0100

    Implemented a basic server

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,52 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private final int PORT = 18718;
+
+    private List<Game> games; //TODO: Map
+    private int tempPlayerCount = 0;
+
+
+    public Server() {
+        games = new ArrayList<>();
+    }
+
+    public void createGame() {
+        games.add(new Game());
+    }
+
+    public void joinGame(Socket client, int index) {
+        games.get(index).makePlayer("Player " + ++tempPlayerCount, client);
+    }
+
+    public void startServer() {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                if (games.isEmpty()) {
+                    createGame();
+                }
+                joinGame(clientSocket, 0);
+                if (tempPlayerCount >= 2) {
+                    games.get(0).start();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.createGame();
+        server.startServer();
+    }
+
 }
