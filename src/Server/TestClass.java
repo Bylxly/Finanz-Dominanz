5d684b8e9886b9c239c37b47ddf514f3ced60c35
timburke-lehmann <timburke@gmx.de>
2024-11-19 18:52:07 +0100

    Not needed anymore with the client server architecture

--- src/Server/TestClass.java
+++ src/Server/TestClass.java
@@ -3,13 +3,13 @@ package Server;
 public class TestClass {
 
     public static void main(String[] args) {
-        Game game = new Game();
+        /*Game game = new Game();
 
         game.makePlayer("Nikolas");
         game.makePlayer("Samuel");
         game.makePlayer("Tim");
 
         game.printBoard();
-        game.startGame();
+        game.startGame();*/
     }
 }
