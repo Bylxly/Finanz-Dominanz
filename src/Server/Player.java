b47e2a9e5ec141ef81c29987b300e4e129fcda12
timburke-lehmann <timburke@gmx.de>
2024-11-19 18:48:06 +0100

    added client socket; Also added an Output Stream and a method to send a message to the client

--- src/Server/Player.java
+++ src/Server/Player.java
@@ -4,20 +4,44 @@ package Server;
 import Server.Field.Field;
 import Server.Field.Property.Property;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public class Player implements Serializable {
 
     private int money;
     private final String name;
     private Field currentField;
     private List<Property> properties;
+    private transient Socket client;
+    private transient ObjectOutputStream objectOutputStream;
 
-    public Player(int money, String name) {
+    public Player(int money, String name, Socket client) {
         this.money = money;
         this.name = name;
         properties = new ArrayList<>();
+        this.client = client;
+        try {
+            this.objectOutputStream = new ObjectOutputStream(client.getOutputStream()); // Stream initialisieren
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(Object message) {
+        try {
+            if (objectOutputStream != null) {
+                objectOutputStream.reset(); // Puffer zurücksetzen
+                objectOutputStream.writeObject(message);
+                objectOutputStream.flush(); // Sicherstellen, dass die Nachricht gesendet wird
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void takeMoney(int amount) {
@@ -54,5 +78,9 @@ public class Player {
 
     public void removeProperty(Property property) {
         properties.remove(property);
+    }
+
+    public Socket getClient() {
+        return client;
     }
 }
