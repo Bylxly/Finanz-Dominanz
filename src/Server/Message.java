d8ac0c9251f3190f02e2832d5fa166fc0235ca43
timburke-lehmann <timburke@gmx.de>
2024-11-21 13:33:33 +0100

    changed message to String and added messageType

--- src/Server/Message.java
+++ src/Server/Message.java
@@ -3,13 +3,19 @@ package Server;
 import java.io.Serializable;
 
 public class Message implements Serializable {
-    private final MsgType message;
+    private final MsgType messageType;
+    private final String message;
 
-    Message(MsgType message) {
+    Message(MsgType messageType, String message) {
+        this.messageType = messageType;
         this.message = message;
     }
 
-    public MsgType getMessage() {
+    public MsgType getMessageType() {
+        return messageType;
+    }
+
+    public String getMessage() {
         return message;
     }
 }
