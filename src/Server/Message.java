764c569baf93fd7755576a89de17a456552397a8
timburke-lehmann <timburke@gmx.de>
2024-11-21 10:47:52 +0100

    made message serializable

--- src/Server/Message.java
+++ src/Server/Message.java
@@ -1,6 +1,8 @@
 package Server;
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
     private String message;
 
     Message(String message) {
