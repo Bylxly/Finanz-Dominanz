6dcefe585ae9e55859e2259296a4eea34ab5ff6e
plankeeee <skeley007@gmail.com>
2024-11-28 11:01:20 +0100

    improved code

--- src/Server/Field/AbInKnast.java
+++ src/Server/Field/AbInKnast.java
@@ -4,12 +4,19 @@ import Server.Player;
 
 public class AbInKnast extends Field {
 
+    private boolean areYouArrested = false;
+
     public AbInKnast(String name) {
         super(name);
     }
 
     @Override
     public boolean startAction(Player p) {
+        areYouArrested = true;
         return true;
+    }
+
+    public boolean getAreYouArrested() {
+        return areYouArrested;
     }
 }
--- src/Server/Field/Property/Knast.java
+++ src/Server/Field/Property/Knast.java
@@ -1,11 +1,10 @@
 package Server.Field.Property;
 
+import Server.Field.AbInKnast;
 import Server.GameUtilities;
 import Server.Player;
 
 public class Knast extends Property {
-
-    private boolean arrested;
 
     public Knast(String name, int price, int[] rent, int hypothek) {
         super(name, price, rent, hypothek);
@@ -17,15 +16,7 @@ public class Knast extends Property {
     }
 
     @Override
-    public boolean startAction(Player player) {
-        return isArrested();
-    }
-
-    public void setArrested(boolean arrest) {
-        this.arrested = arrest;
-    }
-
-    public boolean isArrested() {
-        return arrested;
+    public boolean startAction(Player p) {
+        return false;
     }
 }
