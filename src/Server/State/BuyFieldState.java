17ea8e3040a387b848c88c8b9c39705a06d0e8cf
timburke-lehmann <timburke@gmx.de>
2024-12-11 20:21:25 +0100

    Knast will now also be auctioned

--- src/Server/State/BuyFieldState.java
+++ src/Server/State/BuyFieldState.java
@@ -32,6 +32,9 @@ public class BuyFieldState implements GameState {
             }
         }
         else if (currentProperty instanceof Knast) {
+            game.setCurrentGameState(new AuctionState(game));
+            game.getCurrentGameState().execute();
+
             game.setCurrentGameState(new ExecuteFieldState(game));
             game.getCurrentGameState().execute();
         }
