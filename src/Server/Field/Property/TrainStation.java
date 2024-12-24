8df7c6e4817c3e50a4a0e41d824d9d7aa002e0b5
timburke-lehmann <timburke@gmx.de>
2024-12-24 18:38:13 +0000

    changed class variable anzahlDerBesitzendenBahnhöfeEinesSpielers to a local variable

--- src/Server/Field/Property/TrainStation.java
+++ src/Server/Field/Property/TrainStation.java
@@ -5,13 +5,12 @@ import Server.Player;
 
 public class TrainStation extends Property {
 
-    public int anzahlDerBesitzendenBahnhöfeEinesSpielers = 0;
-
     public TrainStation(String name, int price, int[] rent, int hypothek) {
         super(name, price, rent, hypothek);
     }
 
     public int getAnzahlBahnhöfe() {
+        int anzahlDerBesitzendenBahnhöfeEinesSpielers = 0;
         for(Property property : getOwner().getProperties()){
             if(property instanceof TrainStation){
                 anzahlDerBesitzendenBahnhöfeEinesSpielers++;
