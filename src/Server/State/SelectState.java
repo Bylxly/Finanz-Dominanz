87a867f3c1365154ede842281b38e8fe822bec9d
timburke-lehmann <timburke@gmx.de>
2024-12-27 21:33:57 -0600

    Refactor property selection to support generic objects.
    Updated logic to handle generic objects instead of restricting to properties

--- src/Server/State/SelectState.java
+++ src/Server/State/SelectState.java
@@ -4,6 +4,7 @@ import Server.Field.Property.Property;
 import Server.Game;
 import Server.Message;
 import Server.MsgType;
+import Server.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,49 +20,77 @@ public abstract class SelectState implements GameState {
 
     /**
      * Abstrakte Methode, die von Unterklassen implementiert wird.
-     * Definiert, welche Grundstücke für die Aktion infrage kommen.
+     * Definiert, welche Objekte für die Aktion infrage kommen.
      */
-    protected abstract Map<Integer, ? extends Property> getEligibleProperties();
+    protected abstract Map<Integer, Object> getEligibleObjects();
 
     /**
-     * Abstrakte Methode, die die spezifische Aktion auf der gewählten Eigenschaft ausführt.
+     * Abstrakte Methode, die die spezifische Aktion auf dem gewählten Objekt ausführt.
      */
-    protected abstract void performAction(Property property);
+    protected abstract void performAction(Object object);
 
     @Override
     public void execute() {
-        Map<Integer, ? extends Property> properties = getEligibleProperties(); // Filterlogik wird von Unterklassen definiert.
-
-        game.getActivePlayer().sendObject(new Message(MsgType.SELECT_PROPERTY, null));
-        if (properties.isEmpty()) {
-            game.getActivePlayer().sendObject(new Message(MsgType.INFO, getNoSuitablePropertiesMessage()));
-        } else {
-            List<String> message = new ArrayList<>();
-            message.add(getSelectPropertyMessage());
-            for (Integer index : properties.keySet()) {
-                message.add(index + ": " + properties.get(index).getName() + generatePropertyInfoMessage(index, properties));
-            }
-            message.add("Quit: Quit this menu");
-            game.getActivePlayer().sendObject(message);
-
-            // Spieler wählt eine Grundstück aus.
-            int index = Integer.parseInt(game.getActivePlayer().recieveMessage());
-
-            // If player selects quit
-            if (index == -1) return;
-
-            Property selectedProperty = properties.get(index);
-            if (selectedProperty != null && game.getActivePlayer().getProperties().contains(selectedProperty)) {
-                performAction(selectedProperty); // Aktion auf dem gewählten Grundstück durchführen.
-                game.getActivePlayer().sendObject(game); // Aktualisiertes Spielobjekt senden.
-            }
+        Object object = selectObject();
+        if (object != null) {
+            performAction(object);
         }
     }
 
-    public abstract String getNoSuitablePropertiesMessage();
+    public Object selectObject() {
+        Map<Integer, Object> objectMap = getEligibleObjects(); // Filterlogik wird von Unterklassen definiert.
+
+        game.getActivePlayer().sendObject(new Message(MsgType.SELECT_OBJECT, null));
+        if (objectMap.isEmpty()) {
+            game.getActivePlayer().sendObject(new Message(MsgType.INFO, getNoSuitableObjectsMessage()));
+            return null;
+        } else {
+            List<String> message = new ArrayList<>();
+            message.add(getSelectPropertyMessage());
+
+            Class<?> instaceOf = getInstanceOf(objectMap);
+
+            if (Property.class.isAssignableFrom(instaceOf)) {
+                for (Integer index : objectMap.keySet()) {
+                    message.add(index + ": " + ((Property)objectMap.get(index)).getName() + generateObjectInfoMessage(index, objectMap));
+                }
+            }
+            else if (Player.class.isAssignableFrom(instaceOf)) {
+                for (Integer index : objectMap.keySet()) {
+                    message.add(index + ": " + ((Player)objectMap.get(index)).getName());
+                }
+            }
+            message.add("Quit: Quit this menu");
+            message.add(getChooseMessage());
+            game.getActivePlayer().sendObject(message);
+
+            // Spieler wählt ein Objekt aus.
+            int index = Integer.parseInt(game.getActivePlayer().recieveMessage());
+
+            // If player selects quit
+            if (index == -1) return null;
+
+            return objectMap.get(index);
+        }
+    }
+
+    public Class<?> getInstanceOf(Map<Integer, Object> objectMap) {
+        Class<?> instaceOf = objectMap.get(1).getClass();
+        for (Object object : objectMap.values()) {
+            if (!object.getClass().equals(instaceOf)) {
+                instaceOf = null;
+                break;
+            }
+        }
+        return instaceOf;
+    }
+
+    public abstract String getNoSuitableObjectsMessage();
 
     public abstract String getSelectPropertyMessage();
 
-    public abstract String generatePropertyInfoMessage(int index, Map<Integer, ? extends Property> properties);
+    public abstract String generateObjectInfoMessage(int index, Map<Integer, Object> objectMap);
+
+    public abstract String getChooseMessage();
 
 }
