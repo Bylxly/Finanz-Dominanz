a1a608c741707748c3e071d95360b2b33e6522c5
timburke-lehmann <timburke@gmx.de>
2025-01-09 14:09:35 +0100

    removed unneeded methods

--- src/Server/GameUtilities.java
+++ src/Server/GameUtilities.java
@@ -2,8 +2,6 @@ package Server;
 
 import Server.Field.Property.Property;
 
-import java.util.ArrayList;
-
 public class GameUtilities {
 
     public static void transferMoney(Player fromPlayer, Player toPlayer, int amount){
@@ -24,14 +22,6 @@ public class GameUtilities {
     public static void receiveFromBank(Player p, int amount){
         p.giveMoney(amount);
     }
-
-    /*public void shuffleCards(ArrayList<Card> cards){
-
-    }
-
-    public void drawCard(ArrayList<Card> cards){
-
-    }*/
 
     public static boolean checkIfEnoughMoney(Player p, int amount){
         return p.getMoney() >= amount;
