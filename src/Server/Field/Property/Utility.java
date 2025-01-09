5613dba8acf2893fd22aec769f040f41e518e421
timburke-lehmann <timburke@gmx.de>
2025-01-09 14:08:53 +0100

    implemented that you have to roll to get the amount you need to pay when on utilities

--- src/Server/Field/Property/Utility.java
+++ src/Server/Field/Property/Utility.java
@@ -1,33 +1,42 @@
 package Server.Field.Property;
 
-import Server.GameUtilities;
-import Server.Player;
+import Server.*;
 
 public class Utility extends Property {
 
+    private Game game;
 
-    public Utility(String name, int price, int[] rent, int hypothek) {
-        super(name, price, rent, hypothek);
+    public Utility(String name, int price, int hypothek, Game game) {
+        super(name, price, null, hypothek);
+        this.game = game;
     }
 
     @Override
     public void payRent(Player player) {
+        player.sendObject(new Message(MsgType.INFO, "Roll to determine the amount you need to pay for rent"));
+        if (getAnzahlWerkeOwnedByOwner(getOwner()) == 1) {
+            payRentTimes(player, 4);
+        }
+        else if (getAnzahlWerkeOwnedByOwner(getOwner()) == 2) {
+            payRentTimes(player, 10);
+        }
+    }
 
+    public void payRentTimes(Player player, int times) {
+        game.askRoll(player);
+        int roll = game.getRoll().getTotal();
+        GameUtilities.transferMoney(player, getOwner(), roll * times);
     }
 
     @Override
     public boolean startAction(Player player) {
         if (!hasHypothek()) {
-            if (GameUtilities.checkIfEnoughMoney(player, getRent(getAnzahlWerkeOwnedByPlayer(getOwner()) - 1))) {
-                payRent(player);
-                return true;
-            }
-            return false;
+            payRent(player);
         }
         return true;
     }
 
-    public int getAnzahlWerkeOwnedByPlayer(Player player) {
+    public int getAnzahlWerkeOwnedByOwner(Player player) {
         int anzahlWerkeOwnedByPlayer = 0;
         for (Property property : player.getProperties()) {
             if (property instanceof Utility) {
